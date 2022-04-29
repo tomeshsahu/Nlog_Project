1c76f2e4a04ff3c9dfc3f7c2f4a85522f79763e7
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-29 10:54:18 +0530

    [TOMESHWAR]ADD:- N LOG

---
+++ NLog_Example/AddNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace NLog_Example
+{
+    public class AddNumber
+    {
+        NLog obj1 = new NLog();
+        public int Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                obj1.LogDebug("Debug Successfull : Sum()");
+                obj1.LogError("Expecting null values");
+                obj1.LogWarn("a or b should not equal to 0");
+            }
+            int c = a + b;
+            obj1.LogDebug("Debug Successfull : Sum()");
+            obj1.LogInfo("Sum method passed, Result" + c);
+            return c;
+        }
+    }
+}
+
+
---
+++ NLog_Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NLog_Example;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Welcome To Nlogger");
+        AddNumber add = new AddNumber();
+
+        Console.WriteLine($"Sum of Two Number is= {add.Sum(1, 2)}");
+
+    }
+}
